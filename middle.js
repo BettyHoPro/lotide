b54b475369e361d5a4288916b170e78bbfd30d02
BettyHoPro <bettyvisualtech@gmail.com>
2021-01-12 06:03:59 +0000

    middle refactpry

--- middle.js
+++ middle.js
@@ -1,24 +1,5 @@
-const eqArrays = function (arr1, arr2){
-  if ( arr1.length !== arr2.length ){
-    return false;
-  } else {
-    for (let q=0 ; q< arr1.length; q++){
-      if ( arr1[q] !== arr2[q]) {
-        return false ;
-      }
-    }
-    return true;
-  }
-}
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Array Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑 Array Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-/////////// func /////////
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
 middle = ( array ) => { 
   let arrMiddle = [ ];
   let midNum = array.length/2;
@@ -34,16 +15,6 @@ middle = ( array ) => {
   }
   return arrMiddle;
 }
+module.exports = middle;
 
 
-
-//////// test code //////
-//For arrays with one or two elements, there is no middle. Return an empty array.
-console.log(middle([1])) // => []
-console.log(middle([1, 2])) // => []
-//For arrays with odd number of elements, an array containing a single middle element should be returned.
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
-//For arrays with an even number of elements, an array containing the two elements in the middle should be returned
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
