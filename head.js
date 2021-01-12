00b6c80711b3edf790d924fbcd32e38aa1ee05ee
BettyHoPro <bettyvisualtech@gmail.com>
2021-01-12 05:38:15 +0000

    update the head.js refactory

--- head.js
+++ head.js
@@ -2,7 +2,8 @@ const assertEqual = require('./assertEqual');
 const head=function(array1){
   return array1[0];
 };
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports= head ;
--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,5 @@
 const assertEqual = require('../assertEqual');
-const head=function(array1){
-  return array1[0];
-};
+const head=require('../head');
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual(head([5,6,7]), 5);
